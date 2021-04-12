641f6e9ce64891fab7e36507084ac6c8d7f81d7f
thatblindgeye <thatblindgeye@gmail.com>
2021-04-12 13:32:00 -0400

    Fix issue where incorrect text was read by screen readers for unit button

--- src/scripts/unit-utilities.js
+++ src/scripts/unit-utilities.js
@@ -3,21 +3,22 @@ function saveUnitType(unit) {
 }
 
 const unitButton = document.getElementById('unit-btn');
+const unitButtonLabel = document.getElementById('unit-btn-label');
 function changeUnitType() {
   if (localStorage.getItem('units') === 'metric') {
-    unitButton.textContent = 'Imperial';
+    unitButton.textContent = `Imperial Units`;
     saveUnitType('imperial');
   } else {
-    unitButton.textContent = 'Metric';
+    unitButton.textContent = 'Metric Units ';
     saveUnitType('metric');
   }
 }
 
 function loadUnitType() {
   if (localStorage.getItem('units') === 'metric') {
-    unitButton.textContent = 'Metric';
+    unitButton.textContent = 'Metric Units';
   } else {
-    unitButton.textContent = 'Imperial';
+    unitButton.textContent = 'Imperial Units';
   }
 }
 
